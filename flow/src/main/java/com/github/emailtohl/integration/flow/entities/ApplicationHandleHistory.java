01a9e4b5c134aafedb93bf22521509eae13b96ee
HeLei <emailtohl@163.com>
2017-06-17 11:25:14 +0800

    flow init

---
+++ flow/src/main/java/com/github/emailtohl/integration/flow/entities/ApplicationHandleHistory.java
@@ -0,0 +1,79 @@
+package com.github.emailtohl.integration.flow.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.github.emailtohl.integration.common.jpa.entity.BaseEntity;
+import com.github.emailtohl.integration.flow.entities.ApplicationForm.Status;
+import com.github.emailtohl.integration.user.entities.User;
+/**
+ * 申请表的历史处理记录
+ * @author HeLei
+ * @date 2017.02.04
+ */
+@org.hibernate.annotations.BatchSize(size = 10)// 因n+1查询问题，盲猜优化，一次性加载size个代理
+@Entity
+@Table(name = "t_application_handle_history")
+public class ApplicationHandleHistory extends BaseEntity {
+	private static final long serialVersionUID = -352345718622378836L;
+	private ApplicationForm applicationForm;
+	private User handler;
+	private String cause;
+	private Status status;
+	
+	public ApplicationHandleHistory() {
+		super();
+	}
+	
+	public ApplicationHandleHistory(ApplicationForm applicationForm, User handler, String cause, Status status) {
+		super();
+		this.applicationForm = applicationForm;
+		this.handler = handler;
+		this.cause = cause;
+		this.status = status;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "application_form_id", nullable = false)
+	public ApplicationForm getApplicationForm() {
+		return applicationForm;
+	}
+	public void setApplicationForm(ApplicationForm applicationForm) {
+		this.applicationForm = applicationForm;
+	}
+	
+	@ManyToOne
+	@JoinColumn(name = "handler_id")
+	public User getHandler() {
+		return handler;
+	}
+	public void setHandler(User handler) {
+		this.handler = handler;
+	}
+	
+	public String getCause() {
+		return cause;
+	}
+	public void setCause(String cause) {
+		this.cause = cause;
+	}
+	
+	@Enumerated(EnumType.STRING)
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+	
+	@Override
+	public String toString() {
+		return "ApplicationHandleHistory [applicationForm name=" + applicationForm.getName() + ", handler=" + handler.getEmail() + ", cause="
+				+ cause + ", status=" + status + "]";
+	}
+
+}
