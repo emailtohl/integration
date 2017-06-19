83691fb80b55a42d3ea21b2f9b8681722b644826
HeLei <emailtohl@163.com>
2017-06-19 09:49:37 +0800

    update

---
+++ user/src/main/java/com/github/emailtohl/integration/user/service/RoleService.java
@@ -0,0 +1,93 @@
+package com.github.emailtohl.integration.user.service;
+
+import static com.github.emailtohl.integration.user.entities.Authority.USER_ROLE_AUTHORITY_ALLOCATION;
+import static com.github.emailtohl.integration.user.service.UserService.CACHE_NAME_USER;
+import static com.github.emailtohl.integration.user.service.UserService.CACHE_NAME_USER_PAGER;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+
+import com.github.emailtohl.integration.user.entities.Authority;
+import com.github.emailtohl.integration.user.entities.Role;
+/**
+ * 角色管理的服务层
+ * @author HeLei
+ * @date 2017.02.04
+ */
+@PreAuthorize("hasAuthority('" + USER_ROLE_AUTHORITY_ALLOCATION + "')")
+@Transactional
+@Validated
+public interface RoleService {
+	String CACHE_NAME_ROLE = "roleCache";
+	String CACHE_NAME_ROLE_LIST = "roleListCache";
+	String CACHE_NAME_AUTHORITY_LIST = "authorityListCache";
+	/**
+	 * 获取角色
+	 */
+	@Cacheable(value = CACHE_NAME_ROLE, key = "#root.args[0]")
+	Role getRole(long id);
+	
+	/**
+	 * 获取所有角色
+	 * @return
+	 */
+	@Cacheable(value = CACHE_NAME_ROLE_LIST)
+	List<Role> getRoles();
+	
+	/**
+	 * 获取所有权限
+	 * @return
+	 */
+	@Cacheable(value = CACHE_NAME_AUTHORITY_LIST)
+	List<Authority> getAuthorities();
+	
+	/**
+	 * 新增一个角色
+	 * 角色和用户的关系有程序内部控制，创建时不做关联
+	 * @param role
+	 * @return
+	 */
+	@CacheEvict(value = { CACHE_NAME_ROLE, CACHE_NAME_ROLE_LIST }, allEntries = true)
+	long createRole(@Valid Role role);
+	
+	/**
+	 * 对long createRole(@Valid Role role);的封装，便于控制器调用
+	 * @param role 包含角色的基本信息
+	 * @param authorityNames 角色含有的权限名字
+	 * @return
+	 */
+	@CacheEvict(value = { CACHE_NAME_ROLE, CACHE_NAME_ROLE_LIST }, allEntries = true)
+	long createRole(@Valid Role role, Set<String> authorityNames);
+	
+	/**
+	 * 修改角色
+	 * 角色和用户的关系有程序内部控制，修改时不做关联，也就是说不会修改用户和角色的关系
+	 * @param id
+	 * @param role
+	 */
+	@CacheEvict(value = { CACHE_NAME_ROLE, CACHE_NAME_ROLE_LIST, CACHE_NAME_USER, CACHE_NAME_USER_PAGER }, allEntries = true)
+	void updateRole(long id, @Valid Role role);
+	
+	/**
+	 * 为角色分配权限
+	 * @param roleId 角色ID
+	 * @param authorityNames 权限名
+	 */
+	@CacheEvict(value = { CACHE_NAME_ROLE, CACHE_NAME_ROLE_LIST, CACHE_NAME_USER, CACHE_NAME_USER_PAGER }, allEntries = true)
+	void grantAuthorities(long roleId, Set<String> authorityNames);
+	
+	/**
+	 * 删除角色
+	 * @param id
+	 */
+	@CacheEvict(value = { CACHE_NAME_ROLE, CACHE_NAME_ROLE_LIST, CACHE_NAME_USER, CACHE_NAME_USER_PAGER }, allEntries = true)
+	void deleteRole(long id);
+}
---
+++ user/src/test/java/com/github/emailtohl/integration/user/service/RoleServiceImplTest.java
@@ -0,0 +1,138 @@
+package com.github.emailtohl.integration.user.service;
+
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.cache.CacheManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.github.emailtohl.integration.common.exception.ResourceNotFoundException;
+import com.github.emailtohl.integration.user.dao.AuthorityRepository;
+import com.github.emailtohl.integration.user.dao.CleanAuditData;
+import com.github.emailtohl.integration.user.entities.Authority;
+import com.github.emailtohl.integration.user.entities.Employee;
+import com.github.emailtohl.integration.user.entities.Role;
+import com.github.emailtohl.integration.user.entities.User;
+import com.github.emailtohl.integration.user.userTestConfig.DataSourceConfiguration;
+import com.github.emailtohl.integration.user.userTestConfig.ServiceConfiguration;
+
+/**
+ * 业务类测试
+ * @author HeLei
+ * @date 2017.02.04
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ServiceConfiguration.class)
+@ActiveProfiles(DataSourceConfiguration.H2_RAM_DB)
+public class RoleServiceImplTest {
+	static final Logger logger = LogManager.getLogger();
+	@Inject @Named("userServiceImpl") UserService userService;
+	@Inject SecurityContextManager securityContextManager;
+	@Inject RoleService roleService;
+	@Inject AuthorityRepository authorityRepository;
+	@Inject CacheManager cacheManager;
+	@Inject CleanAuditData cleanAuditData;
+	
+	Employee u;
+	Role r;
+	String roleName = "roleTest";
+	Authority auth1, auth2;
+	Long userId, roleId, auth1Id, auth2Id;
+
+	@Before
+	public void setUp() throws Exception {
+		securityContextManager.setEmailtohl();
+		
+		u = new Employee();
+		r = new Role();
+		
+		auth1 = new Authority("authorityTest1", "测试权限1的描述");
+		auth2 = new Authority("authorityTest2", "测试权限2的描述");
+		
+		u.setName("userEmailTest");
+		u.setEmail("userEmailTest@test.com");
+		u.setPassword("123456");
+		
+		r.setName(roleName);
+		r.setDescription("test");
+		
+		r.getAuthorities().add(auth1);
+		auth1.getRoles().add(r);
+		
+		authorityRepository.save(auth1);
+		authorityRepository.save(auth2);
+		auth1Id = auth1.getId();
+		auth2Id = auth2.getId();
+		
+		// 注意：在新建用户时，接口只授予Employee角色，所以需要创建好了User后再授予角色
+		roleId = roleService.createRole(r);
+		userId = userService.addEmployee(u).getId();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		roleService.deleteRole(roleId);
+		cleanAuditData.cleanRoleAudit(roleId);
+
+		userService.deleteUser(userId);
+		cleanAuditData.cleanUserAudit(userId);
+
+		authorityRepository.delete(auth1Id);
+		cleanAuditData.cleanAuthorityAudit(auth1Id);
+		authorityRepository.delete(auth2Id);
+		cleanAuditData.cleanAuthorityAudit(auth2Id);
+		
+//		Cache c = cacheManager.getCache(AuthorityRepository.CACHE_NAME);
+//		c.clear();
+	}
+
+	@Test
+	public void testGet() {
+		assertNotNull(roleService.getRole(roleId));
+		assertFalse(roleService.getRoles().isEmpty());
+		assertTrue(roleService.getAuthorities().contains(auth1));
+		assertTrue(roleService.getAuthorities().contains(auth2));
+	}
+
+	@Test
+	public void testUpdateAndCache() throws ResourceNotFoundException {
+		// 先执行带缓存的方法，使其缓存上
+		User tu = userService.getUserByEmail(u.getEmail());
+		// 初创用户时，只有Employee权限
+		boolean flag = tu.getRoles().parallelStream().anyMatch(tr -> Role.EMPLOYEE.equals(tr.getName()));
+		assertTrue(flag);
+		
+		// 为其授予新的角色
+		userService.grantRoles(tu.getId(), roleName);
+		tu = userService.getUserByEmail(u.getEmail());
+		// 用户的角色变了，查看是否清空缓存重新加载最新状态
+		flag = tu.getRoles().parallelStream().anyMatch(tr -> roleName.equals(tr.getName()));
+		assertTrue(flag);
+		
+		String newRoleName = "updateRole";
+		Role updateRole = new Role(newRoleName, "for test");
+		// 更新角色名字
+		roleService.updateRole(roleId, updateRole);
+		// 再次加载user
+		tu = userService.getUserByEmail(u.getEmail());
+		flag = tu.getRoles().parallelStream().anyMatch(tr -> roleName.equals(tr.getName()));
+		// 不再包含原先的"roleTest"的角色名
+		assertFalse(flag);
+		flag = tu.getRoles().parallelStream().anyMatch(tr -> newRoleName.equals(tr.getName()));
+		// 包含新的角色名，证明缓存被清除
+		assertTrue(flag);
+	}
+}
