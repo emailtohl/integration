bf07882e304154b5afff1a8b5f0de63345c87482
HeLei <emailtohl@163.com>
2017-06-23 15:18:41 +0800

    update

---
+++ web/src/main/webapp/site/forum/add/ctrl.js
@@ -0,0 +1,64 @@
+define(['forum/module', 'common/module', 'forum/service', 'ckeditor', 'ckeditorConfig'], function(forumModule) {
+	return forumModule
+		.controller('ForumAdd', ['$scope', '$http', '$state', 'forumService', 'util', function($scope, $http, $state, forumService, util) {
+			var self = this, promise;
+			promise = $scope.getAuthentication();
+			//util.loadasync('lib/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css');
+
+			self.forumPost = {};
+
+			// 需要先获取登录信息才能上传图片
+			promise.then(function(data) {
+				/**
+				 * CKEDITOR API手册参考：
+				 * http://docs.ckeditor.com/#!/guide
+				 */
+				// Replace the <textarea id="editor1"> with a CKEditor
+				// instance, using default configuration.
+				CKEDITOR.editorConfig = function(config) {
+					config.language = 'zh';
+					config.uiColor = '#AADC6E';
+				};
+
+				function getUrlWithCsrfParam() {
+					var url = null;
+					// 只有登录了，才有CSRF TOKEN，不然上传文件会报错，如果没登录则屏蔽上传文件功能
+					if($scope.isAuthenticated()) {
+						url = 'forum/image?type=image';
+						var token = util.getCookie('XSRF-TOKEN');
+						if(token) {
+							url += '&_csrf=' + token;
+						}
+					}
+					return url;
+				}
+				var editor = CKEDITOR.replace('editor1', {
+					filebrowserImageUploadUrl: getUrlWithCsrfParam(),
+				});
+				editor.on('change', function(event) {
+					self.forumPost.body = this.getData(); // 内容
+					$scope.$apply();
+				});
+				/*
+				editor.on('fileUploadRequest', function(evt) {
+					var xhr = evt.data.fileLoader.xhr;
+					var token = util.getCookie('XSRF-TOKEN');
+					if (token) {
+						xhr.setRequestHeader('X-XSRF-TOKEN', token);
+					}
+				});
+				*/
+
+			});
+
+			self.submit = function() {
+				self.forumPost.email = $scope.authentication.username;
+				forumService.add(self.forumPost).then(function(resp) {
+					self.forumPost = {};
+					$state.go('forum.search', {}, {
+						reload: true
+					});
+				});
+			}
+		}]);
+});
---
+++ web/src/main/webapp/site/forum/service.js
@@ -0,0 +1,24 @@
+define(['forum/module', 'common/module'], function(forumModule) {
+	return forumModule.factory('forumAddService', ['$http', 'util', function($http, util) {
+		return {
+			getPager: function(page) {
+				var param = page ? '?page=' + page : '';
+				return $http.get('forum/pager' + param);
+			},
+			search: function(query, page) {
+				var param = {
+					query: query,
+					page: page
+				};
+				param = util.encodeUrlParams(param);
+				return $http.get('forum/search' + (param ? '?' + param : ''));
+			},
+			'delete': function(id) {
+				return $http['delete']('forum/' + id);
+			},
+			add: function(forumPost) {
+				return $http.post('forum', forumPost);
+			},
+		};
+	}]);
+});
